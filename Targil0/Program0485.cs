da9922c3b15c104f0a09f539d66037377d7c8921
bmor18 <bmor@g.jct.ac.il>
2021-10-06 16:57:58 +0300

    initial code+ welcome functions

---
+++ Targil0/Program0485.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcomeo485();
+            Welcome5223();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5223();
+        private static void Welcomeo485()
+        {
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", Name);
+        }
+    }
+}
