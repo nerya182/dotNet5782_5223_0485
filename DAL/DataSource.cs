a55553f4f76b5444f4ffc1909456e6ab96ae865d
bmor18 <bmor@g.jct.ac.il>
2021-10-13 12:19:22 +0300

    Added Classes

---
+++ DAL/DataSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+
+
+namespace DalObject
+{
+    public class DataSource
+    {
+        internal class Config
+        {
+            internal static int newCustomerId = 0;
+            internal static int newStationId = 0;
+
+        }
+
+
+        public static Customer[] customers = new Customer[100];
+        public static Station[] stations = new Station[5];
+
+        internal static void Initialize()
+        {
+            CreateStations();
+        }
+        private static void CreateStations()
+        {
+
+        }
+        
+    }
+}
+
